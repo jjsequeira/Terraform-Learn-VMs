d46077b4f6ad455b9093362ab503372e3a619892
jjsequeira <julian.sequeira.18@hotmail.com>
2024-09-19 16:13:35 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@
 provider "azurerm" {
   resource_provider_registrations = "none"
   features {}
- 
-    # Use variables to authenticate
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
 
 
